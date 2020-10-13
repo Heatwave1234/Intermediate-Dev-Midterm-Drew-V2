49a460b1036331c35d2ac9c6f17b5d5f72383af8
Drew Caines <drewcaines@gmail.com>
2020-10-13 00:17:10 -0400

    Created the pile spawn script and staged the scene.

---
+++ Assets/Scripts/MomMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MomMove : MonoBehaviour
+{
+    //GOAL: Move the mother back and forth at a random speed each time
+    //      Raycast towards the pile, detect when it hits
+
+    public GameObject Mom;
+    public float momSpeed;
+
+    public float xMin;
+    public float xMax;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SortManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortManager : MonoBehaviour
+{
+
+    //GOAL: Define an array of game objects where the next object is randomly
+    //      selected out of the different laundry types
+    //      Display the current object and then place it in a "pile" when the correct
+    //      button is pressed. The piles should be at a location where the object's position can
+    //      vary a little but fall within bounds set by the script
+
+    //public GameObject[] Laundry;
+    public int currentLaundryNum;
+    public int laundryCounter;
+    public int laundryMax;
+    public Vector3 pilePos;
+
+    public GameObject currentLaundry;
+
+    public GameObject blueLaundry;
+    public GameObject whiteLaundry;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentLaundry = Laundry[currentLaundryNum];
+        currentLaundryNum = Random.Range(1, 2);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (laundryCounter <= 49)
+        {
+            spawnLaundry();
+        }
+        
+        //currentLaundryNum = Random.Range(1, 2);
+
+        pilePos.x = Random.Range(-.2f, .2f);
+        pilePos.y = Random.Range(-4.5f, -4.1f);
+
+        //winstate: if you run out of laundry you win
+        if (laundryCounter == laundryMax)
+        {
+            currentLaundryNum = 50;
+
+        }
+    }
+
+    void spawnLaundry()
+    {
+        if (currentLaundryNum == 1)
+        {
+            Instantiate(blueLaundry, pilePos, Quaternion.identity);
+            laundryCounter++;
+            currentLaundryNum = Random.Range(1, 3);
+            Debug.Log("spawned blue");
+            pilePos.z = pilePos.z - .1f;
+        }
+        else if (currentLaundryNum == 2)
+        {
+            Instantiate(whiteLaundry, pilePos, Quaternion.identity);
+            laundryCounter++;
+            currentLaundryNum = Random.Range(1, 3);
+            Debug.Log("spawned white");
+            pilePos.z = pilePos.z - .1f;
+        }
+    }
+}
